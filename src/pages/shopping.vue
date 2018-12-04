cff08e933238a434bd978d40edcaea57fcf5b511
ccg <506566603@qq.com>
2018-12-04 16:40:06 +0800

    练习

---
+++ src/pages/shopping.vue
@@ -0,0 +1,15 @@
+<template>
+  <view>
+    我是购物车
+  </view>
+</template>
+<script>
+  import wepy from 'wepy'
+  export default class Set extends wepy.page {
+
+  }
+</script>
+<style lang="less">
+
+</style>
+
